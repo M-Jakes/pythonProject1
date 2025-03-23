63be300f9507e07b95df3184c2e60abcfa96b3b4
M-Jakes <jakesmatyas@gmail.com>
2025-03-23 18:08:57 +0100

    Cheapest product

--- storage.py
+++ storage.py
@@ -13,7 +13,11 @@ products = [
     },
     {
         "price": 30,
+        "name": "BMW R8",
+    },
+    {
         "name": "BMW",
+        "price": 30
     }
 ]
 
@@ -30,7 +34,6 @@ def add_product():
         'name': product_name,
         'price': product_price
     }
-
     products.append(product2)
 
 def search_product():
@@ -47,12 +50,24 @@ def search_product():
     else:
         print("Žádné produkty nenalezeny.")
 
-
 def total_price():
     total = 0
     for product in products:
         total += product['price']
     print("Celková cena všech produktů:", total, "$")
+
+def cheapest_price():
+    min_price = 0
+    cheap_products = []
+    for product in products:
+        if min_price == 0 or product['price'] < min_price:
+            min_price = product['price']
+            cheap_products = [product]
+        elif product['price'] == min_price:
+            cheap_products.append(product)
+    print("Nejlevnější produkt:")
+    for product in cheap_products:
+        print("Název:", product['name'], ", Cena:", product['price'], "$")
 
 def menu():
     print("Vítej ve skladu")
@@ -60,7 +75,8 @@ def menu():
     print("1. Výpis polože")
     print("2. Přidání položky")
     print("3. Hledání položky")
-    print("4. Cena všech produktů\n")
+    print("4. Cena všech produktů")
+    print("5. Nejlevnější produkt\n")
 
     choice = int(input("Volba: "))
 
@@ -85,6 +101,12 @@ def menu():
     elif choice == 4:
         print("Cena všech produktů")
         total_price()
+        print("")
+        menu()
+
+    elif choice == 5:
+        print("Nejlevnější produkt")
+        cheapest_price()
         print("")
         menu()
 
